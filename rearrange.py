e2a155701f5f00396e6c5b323c586474209bed5d
juan <juanpajaro@javeriana.edu.co>
2025-01-27 08:28:20 -0500

    se agregó una función que incluye dataset, framework de pandas para probar funciones

--- rearrange.py
+++ rearrange.py
@@ -1,9 +1,14 @@
 #!usr/bin/env python3
 
 import re
+import pandas as pd
 
 def rearrange_name(name):
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
     if result is None:
         return name
     return "{} {}".format(result[2], result[1])
+
+def load_data(diccionario):
+    datos = pd.DataFrame(diccionario)
+    return datos
