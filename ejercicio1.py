ecd8bc0e57800d6a1b21f67c1b61de72eb75c475
juan <juanpajaro@javeriana.edu.co>
2025-01-28 09:42:18 -0500

    se modifico el ejercicio1 para mejorar la descripcion

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,16 +6,11 @@ def load_data(ubicacion_datos):
     datos = pd.read_csv(ubicacion_datos, sep = '\t')
     return datos
 
-#retornar la media del indice de masa corporal
+#retornar la media de la variable indice de masa corporal
+#INICIO DE CODIGO
 def calcular_imc(serie):
-    return serie.mean()
+    promedio_BMI = None
 
-"""
-datos = load_data('./data/diabetes.txt')
-datos.columns
-#datos.head()
-calcular_imc(datos["BMI"])
-datos_prueba = [1, 2, 3, 4, 5]
-calcular_imc(datos_prueba)
-print(sum(datos_prueba))
-"""
+    return promedio_BMI
+#FIN DE CODIGO
+
